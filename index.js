4ebcf1fb65bfe9343ffa3ca45f76b67bb82ccd14
Ricardo Alegria <ricardo_alegria@siman.com>
2025-01-23 10:33:43 -0600

    added cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const mongoose = require("mongoose");
 
 const CustomerRoute = require("./routes/customer.route");
@@ -8,6 +9,7 @@ const app = express();
 const port = 8080;
 
 //middleware
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
